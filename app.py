31c04f33de97ec7b31aa985082051f028391d88c
dhokdisha22 < GitHub account  email>
2026-09-16 00:19:58 +0530

    Complete Exercise 1.5

--- app.py
+++ app.py
@@ -1,13 +1,22 @@
 import time
 import uuid
 from datetime import datetime, timezone
+from http.server import BaseHTTPRequestHandler, HTTPServer
 
 random_string = str(uuid.uuid4())
 
-while True:
-    timestamp = datetime.now(timezone.utc).isoformat(
-        timespec="milliseconds"
-    ).replace("+00:00", "Z")
+class Handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == "/":
+            self.send_response(200)
+            self.send_header("Content-type", "text/html")
+            self.end_headers()
+            self.wfile.write(
+                f"<h1>Application {random_string}</h1>".encode()
+            )
 
-    print(f"{timestamp}: {random_string}", flush=True)
-    time.sleep(5)
+server = HTTPServer(("0.0.0.0", 3000), Handler)
+
+print("Server running on port 3000", flush=True)
+
+server.serve_forever()
