9ce8e320bafaf024b44c555bf42a42a3d940fe92
dhokdisha22 < GitHub account  email>
2026-09-20 09:50:37 +0530

    Complete Exercise 1.12

--- log-reader/app.py
+++ log-reader/app.py
@@ -1,28 +1,24 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
-
+import os
+import time
+import urllib.request
+IMAGE_PATH = "/data/image.jpg"
+CACHE_TIME = 600
 class Handler(BaseHTTPRequestHandler):
     def do_GET(self):
-        try:
-            with open("/data/log.txt", "r") as file:
-                logs = file.read()
-        except FileNotFoundError:
-            logs = "No logs yet"
-
-        try:
-            with open("/data/count.txt", "r") as file:
-                count = file.read()
-        except FileNotFoundError:
-            count = "0"
-
+        if not os.path.exists(IMAGE_PATH) or time.time() - os.path.getmtime(IMAGE_PATH) > CACHE_TIME:
+            print("Downloading new image...", flush=True)
+            urllib.request.urlretrieve(
+                "https://picsum.photos/1200",
+                IMAGE_PATH
+            )
+        with open(IMAGE_PATH, "rb") as file:
+            image = file.read()
         self.send_response(200)
-        self.send_header("Content-type", "text/plain")
+        self.send_header("Content-Type", "image/jpeg")
+        self.send_header("Content-Length", str(len(image)))
         self.end_headers()
-
-        response = f"Ping-pong count: {count}\n\n{logs}"
-        self.wfile.write(response.encode())
-
+        self.wfile.write(image)
 server = HTTPServer(("0.0.0.0", 3000), Handler)
-
-print("Reader server running on port 3000", flush=True)
-
+print("Image server running on port 3000", flush=True)
 server.serve_forever()
