6c44c61d93204b2e08cf446a1541735826fd6be7
dhokdisha22 < GitHub account  email>
2026-09-20 11:13:34 +0530

    Complete Exercise 2.1

--- log-reader/app.py
+++ log-reader/app.py
@@ -2,58 +2,78 @@ from http.server import BaseHTTPRequestHandler, HTTPServer
 import os
 import time
 import urllib.request
-IMAGE_PATH = "/data/image.jpg"
+
+IMAGE_PATH = '/data/image.jpg'
 CACHE_TIME = 600
+
 todos = [
-    "Learn Kubernetes",
-    "Complete DevOps exercises",
-    "Build a Todo app"
+    'Learn Kubernetes',
+    'Complete DevOps exercises',
+    'Build a Todo app'
 ]
+
 class Handler(BaseHTTPRequestHandler):
     def do_GET(self):
-        if not os.path.exists(IMAGE_PATH) or time.time() - os.path.getmtime(IMAGE_PATH) > CACHE_TIME:
-            print("Downloading new image...", flush=True)
-            urllib.request.urlretrieve(
-                "https://picsum.photos/1200",
-                IMAGE_PATH
-            )
-        todo_list = ""
-        for todo in todos:
-            todo_list += f"<li>{todo}</li>"
-        html = f"""
-        <!DOCTYPE html>
-        <html>
-        <head>
-            <title>Todo App</title>
-        </head>
-        <body>
-            <h1>Todo App</h1>
-            <form>
-                <input type="text" maxlength="140" placeholder="Write a todo">
-                <button type="submit">Send</button>
-            </form>
-            <h2>Todos</h2>
-            <ul>
-                {todo_list}
-            </ul>
-            <h2>Image</h2>
-            <img src="/image.jpg" width="500">
-        </body>
-        </html>
-        """
-        if self.path == "/image.jpg":
-            with open(IMAGE_PATH, "rb") as file:
+        if self.path == '/image.jpg':
+            if not os.path.exists(IMAGE_PATH) or time.time() - os.path.getmtime(IMAGE_PATH) > CACHE_TIME:
+                print('Downloading new image...', flush=True)
+                urllib.request.urlretrieve('https://picsum.photos/1200', IMAGE_PATH)
+
+            with open(IMAGE_PATH, 'rb') as file:
                 image = file.read()
+
             self.send_response(200)
-            self.send_header("Content-Type", "image/jpeg")
-            self.send_header("Content-Length", str(len(image)))
+            self.send_header('Content-Type', 'image/jpeg')
+            self.send_header('Content-Length', str(len(image)))
             self.end_headers()
             self.wfile.write(image)
             return
-        self.send_response(200)
-        self.send_header("Content-Type", "text/html")
+
+        if self.path == '/':
+            response = urllib.request.urlopen('http://ping-pong-svc:3000/pingpong').read().decode()
+            pong_count = response.split()[-1]
+
+            try:
+                with open('/data/log.txt', 'r') as file:
+                    logs = file.read()
+            except FileNotFoundError:
+                logs = 'No logs yet'
+
+            todo_list = ''
+            for todo in todos:
+                todo_list += f'<li>{todo}</li>'
+
+            html = f'''
+            <!DOCTYPE html>
+            <html>
+            <head>
+                <title>Todo App</title>
+            </head>
+            <body>
+                <h1>Todo App</h1>
+                <p>Ping / Pongs: {pong_count}</p>
+                <pre>{logs}</pre>
+                <form>
+                    <input type='text' maxlength='140' placeholder='Write a todo'>
+                    <button type='submit'>Send</button>
+                </form>
+                <h2>Todos</h2>
+                <ul>{todo_list}</ul>
+                <h2>Image</h2>
+                <img src='/image.jpg' width='500'>
+            </body>
+            </html>
+            '''
+
+            self.send_response(200)
+            self.send_header('Content-Type', 'text/html')
+            self.end_headers()
+            self.wfile.write(html.encode())
+            return
+
+        self.send_response(404)
         self.end_headers()
-        self.wfile.write(html.encode())
-server = HTTPServer(("0.0.0.0", 3000), Handler)
-print("Todo server running on port 3000", flush=True)
+
+server = HTTPServer(('0.0.0.0', 3000), Handler)
+print('Todo server running on port 3000', flush=True)
 server.serve_forever()
--- ping-pong/app.py
+++ ping-pong/app.py
@@ -6,19 +6,17 @@ class Handler(BaseHTTPRequestHandler):
     def do_GET(self):
         global count
 
-        if self.path == "/pingpong":
+        if self.path == '/pingpong':
             self.send_response(200)
-            self.send_header("Content-type", "text/plain")
+            self.send_header('Content-type', 'text/plain')
             self.end_headers()
 
-            with open("/data/count.txt", "w") as file:
-                file.write(str(count))
+            self.wfile.write(f'pong {count}'.encode())
 
-            self.wfile.write(f"pong {count}".encode())
             count += 1
 
-server = HTTPServer(("0.0.0.0", 3000), Handler)
+server = HTTPServer(('0.0.0.0', 3000), Handler)
 
-print("Server running on port 3000", flush=True)
+print('Server running on port 3000', flush=True)
 
 server.serve_forever()
