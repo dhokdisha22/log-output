5a2b115a86c46e7b92617b549b171c4d33186d63
dhokdisha22 < GitHub account  email>
2026-09-23 11:51:11 +0530

    Complete Exercise 2.7

--- ping-pong/app.py
+++ ping-pong/app.py
@@ -1,23 +1,51 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import os
-
-count = 0
-
+import psycopg
+DB_HOST = os.environ["POSTGRES_HOST"]
+DB_NAME = os.environ["POSTGRES_DB"]
+DB_USER = os.environ["POSTGRES_USER"]
+DB_PASSWORD = os.environ["POSTGRES_PASSWORD"]
+DB_PORT = os.environ["POSTGRES_PORT"]
+def get_connection():
+    return psycopg.connect(
+        host=DB_HOST,
+        dbname=DB_NAME,
+        user=DB_USER,
+        password=DB_PASSWORD,
+        port=DB_PORT
+    )
+def initialize_database():
+    with get_connection() as conn:
+        with conn.cursor() as cur:
+            cur.execute("""
+                CREATE TABLE IF NOT EXISTS counter (
+                    id INTEGER PRIMARY KEY,
+                    count INTEGER NOT NULL
+                )
+            """)
+            cur.execute("""
+                INSERT INTO counter (id, count)
+                VALUES (1, 0)
+                ON CONFLICT (id) DO NOTHING
+            """)
 class Handler(BaseHTTPRequestHandler):
     def do_GET(self):
-        global count
-
         if self.path == '/pingpong':
+            with get_connection() as conn:
+                with conn.cursor() as cur:
+                    cur.execute("SELECT count FROM counter WHERE id = 1")
+                    count = cur.fetchone()[0]
+                    cur.execute(
+                        "UPDATE counter SET count = count + 1 WHERE id = 1"
+                    )
             self.send_response(200)
             self.send_header('Content-type', 'text/plain')
             self.end_headers()
-
             self.wfile.write(f'pong {count}'.encode())
-
-            count += 1
-
-server = HTTPServer(("0.0.0.0", int(os.environ["PORT"])), Handler)
-
+initialize_database()
+server = HTTPServer(
+    ("0.0.0.0", int(os.environ["PORT"])),
+    Handler
+)
 print(f"Server running on port {os.environ['PORT']}", flush=True)
-
 server.serve_forever()
